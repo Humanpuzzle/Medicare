5d79e5de8f612fc11a205c44b65a8cbf14243aef
Krisztian Krassalkovics <krisztian.krassalkovics@gmail.com>
2026-09-25 01:18:15 +0200

    fix: remove unused InvalidArgumentException import in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
-use InvalidArgumentException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
